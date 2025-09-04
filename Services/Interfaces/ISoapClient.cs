452374a60aa7eaa162700b974a03ea0c96701d2f
zaidoomoh <zaidoo2002.moh@gmail.com>
2025-09-04 17:23:15 +0300

    9-4

--- Services/Interfaces/ISoapClient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Order_Management_System.Services.Interfaces;
-
-public interface ISoapClient
-{
-    Task<object> GetPatientsAsync(string query, string password);
-    Task<object> ExecuteQueryAsync(string query, string password);
-    Task<object> ExecuteQueryWithParametersAsync(string query, object parameters, string password);
-}
-
